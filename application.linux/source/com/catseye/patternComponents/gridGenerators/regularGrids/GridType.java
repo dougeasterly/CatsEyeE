edc540d6fbb5aa402c5ae8ea7464bd402e877f5d
Eliot Slevin <Eliot.slevin@gmail.com>
2014-12-21 12:48:38 +1300

    Added Github Page

--- application.linux/source/com/catseye/patternComponents/gridGenerators/regularGrids/GridType.java
+++
@@ -1,6 +0,0 @@
-package com.catseye.patternComponents.gridGenerators.regularGrids;
-
-public enum GridType {
-	 HEXAGON, TRIANGLE, SQUARE, VORONOI, DELAUNAY;
-	 public static String[] typeArray = {"HEXAGON", "TRIANGLE", "SQUARE", "VORONOI", "DELAUNAY"};
-}
--- application.linux/source/com/catseye/patternComponents/polygonGenerators/UnitPreview.java
+++
@@ -1,5 +0,0 @@
-package com.catseye.patternComponents.polygonGenerators;
-
-public class UnitPreview {
-	  public static final float SIZE = 120;
-}
